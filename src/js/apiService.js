5c012ced17401994e81eedcf8c2b7f196901692f
Zoia Apostoloska <zoiaapostoloska@gmail.com>
2021-03-12 23:55:13 +0200

    creates plugin

---
+++ src/js/apiService.js
@@ -0,0 +1,30 @@
+export default class ImageApiService {
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+    }
+
+    fetchImages() {
+        const BASE_URL = 'https://pixabay.com/api/';
+        const API_KEY = '20648109-aff5f53cd54a5f40fa07937d6';
+        return fetch(`${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`)
+        .then(response => {
+            if (!response.ok) {
+                throw Error();
+            }
+            return response.json();
+        })
+        .then(({ hits }) => {
+            this.page +=1;
+            return hits})
+    }
+
+    get query() {
+        return this.searchQuery;
+    }
+
+    set query(newQuery){
+        this.searchQuery = newQuery;
+    }
+};
+
---
+++ src/js/createMarkup.js
@@ -0,0 +1,110 @@
+import ImageApiService from "./apiService.js";
+
+
+import photoCardMarkup from '../templates/photo-card-markup.hbs';
+import modal from '../templates/modal.hbs'
+
+import debounce from 'lodash.debounce'
+
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+import { error } from '@pnotify/core';
+import { defaults } from '@pnotify/core';
+defaults.closerHover = false;
+
+const basicLightbox = require('basiclightbox')
+
+
+
+const gallerryRef = document.querySelector('.gallery')
+const searchFormRef = document.querySelector('.search-form');
+const loadBtnRef = document.querySelector('.load-button');
+
+
+
+searchFormRef.addEventListener('input', debounce(imageInputHandler, 1000));
+loadBtnRef.addEventListener('click', onLoadMore);
+
+const imageApiService = new ImageApiService;
+
+function imageInputHandler(event) {
+    imageApiService.searchQuery = event.target.value;
+
+    if (event.target.value === '') {
+        deleteMarkup();
+        return;
+    }
+
+    imageApiService.fetchImages()
+    .then(images => {
+        if(images.length === 0) {
+            deleteMarkup();
+            createError('No matches found. Please try again!');
+            return;
+        }
+
+
+        createMarkup(photoCardMarkup, images);
+        openModal();
+
+        
+    })
+
+function openModal() {
+    const imageRef = document.querySelector('.photo-card')
+        const imgInstance = basicLightbox.create(document.querySelector('.image-big-place'))
+        imageRef.addEventListener('click', imgInstance.show)
+}
+
+};
+
+
+function onLoadMore() {
+    
+    imageApiService.fetchImages()
+    .then(images => {
+        if(images.length === 0) {
+            createError('Sorry! No more pictures. :(');
+            return;
+        }
+        
+        createMarkup(photoCardMarkup, images);
+        scroll();
+    })
+}
+
+
+function createError(errorMessage) {
+    const myError = error({
+        text: errorMessage,
+        animation: 'fade',
+        shadow: true,
+        hide: true,
+        delay: 2000
+      });
+    return myError;
+}
+
+function createMarkup(markupCreationFunction, requestResult) {
+    const markup = markupCreationFunction(requestResult);
+    gallerryRef.innerHTML += markup;
+}
+
+
+function deleteMarkup() {
+    gallerryRef.innerHTML = '';
+}
+
+
+function scroll() {
+    let scrollHeight = Math.max(
+        gallerryRef.scrollHeight, document.documentElement.scrollHeight,
+        gallerryRef.offsetHeight, document.documentElement.offsetHeight,
+        gallerryRef.clientHeight, document.documentElement.clientHeight
+      );
+      window.scrollTo({
+        top: scrollHeight,
+        behavior: 'smooth',
+      });
+}
+
